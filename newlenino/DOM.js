64768bdaef916d7e57b5d339e3a6b59917733f2d
Lenin Compres <lenincompres@gmail.com>
2021-05-26 08:35:53 -0400

    entries

--- newlenino/DOM.js
+++ newlenino/DOM.js
@@ -6,46 +6,41 @@
 
 Element.prototype.create = function (model, ...args) {
   if ([null, undefined].includes(model)) return;
-  let station = args.filter(a => typeof a === 'string')[0]; // style|attr|tag|inner…|on…|name
-  if (['tag', 'onready', 'id'].includes(station)) return;
-  const TAG = this.tagName.toLowerCase();
-  const CLEAR = args.filter(a => typeof a === 'boolean')[0] === true || station === 'content';
-  const IS_PRIMITIVE = ['boolean', 'number', 'string'].includes(typeof model);
-  const PREPEND = args.filter(a => typeof a === 'boolean')[0] === false;
-  if (station === 'content' && TAG === 'meta') station = '*content';
-  if (!station) station = 'content';
-  if (model._bonds) model = model.bind();
-  if (model.binders) return model.binders.forEach(binder => binder.bind(this, station, model.onvalue));
-  if (['text', 'innerText'].includes(station)) return this.innerText = model;
-  if (['html', 'innerHTML'].includes(station)) return this.innerHTML = model;
-  if (station === 'css') return this.css(model);
   if (Array.isArray(model.content)) return model.content.forEach(item => {
     let individual = Object.assign({}, model);
     individual.content = item;
     this.create(individual, ...args);
   });
-  let id;
+  let station = args.filter(a => typeof a === 'string')[0]; // style|attr|tag|inner…|on…|name
+  if (['tag', 'onready', 'id'].includes(station)) return;
+  if (station === 'css') return this.css(model);
+  if (['text', 'innerText'].includes(station)) return this.innerText = model;
+  if (['html', 'innerHTML'].includes(station)) return this.innerHTML = model;
+  if (model._bonds) model = model.bind();
+  if (model.binders) return model.binders.forEach(binder => binder.bind(this, station, model.onvalue));
+  const TAG = this.tagName.toLowerCase();
+  const IS_HEAD = TAG === 'head';
+  const CLEAR = args.filter(a => typeof a === 'boolean')[0] === true || station === 'content';
+  const PREPEND = args.filter(a => typeof a === 'boolean')[0] === false;
+  const IS_PRIMITIVE = DOM.isPrimitive(model);
+  if (station === 'content' && TAG === 'meta') station = '*content'; // disambiguate 
+  if (!station) station = 'content';
   let [tag, ...cls] = station.split('_');
   if (station.includes('.')) {
     cls = station.split('.');
     tag = cls.shift();
   }
+  let id;
   if (tag.includes('#'))[tag, id] = tag.split('#');
   let lowTag = (model.tag ? model.tag : tag).toLowerCase();
-  if (lowTag != tag) id = tag;
+  if (lowTag != tag) id = tag; // camelCase tags are interpreted as id
   tag = lowTag;
   if (model.id) id = model.id;
   let p5Elem = args.filter(a => a && a.elt)[0];
   let elt = model.tagName ? model : model.elt ? model.elt : false;
-  const addID = (id, elt) => {
-    if (Array.isArray(elt)) return elt.forEach(e => addID(id, e));
-    if (!window[id]) return window[id] = elt;
-    if (Array.isArray(window[id])) window[id].push(elt);
-    window[id] = [window[id], elt];
-  }
   if (elt) {
-    if (id) addID(id, elt);
-    else if (tag != elt.tagName.toLowerCase()) addID(tag, elt);
+    if (id) DOM.addID(id, elt);
+    else if (tag != elt.tagName.toLowerCase()) DOM.addID(tag, elt);
     if (cls) cls.forEach(c => c ? elt.classList.add(c) : null);
     return this[PREPEND ? 'prepend' : 'append'](elt);
   }
@@ -61,20 +56,16 @@ Element.prototype.create = function (model, ...args) {
     if (station === 'class') return model.forEach(c => c ? this.classList.add(c) : null);
     if (station === 'addEventListener') return this.addEventListener(...model);
     let map = model.map(s => this.create(s, tag + cls.join('.'), p5Elem, PREPEND ? false : undefined));
-    if (id) addID(id, map);
+    if (id) DOM.addID(id, map);
     return map;
   }
   if (station === 'addEventListener') return model.options ? this.addEventListener(model.type, model.listener, model.options) : this.addEventListener(model.type, model.listener, model.useCapture, model.wantsUntrusted);
-  const IS_HEAD = TAG === 'head';
   if (tag === 'style') {
     if (IS_PRIMITIVE && !IS_HEAD) return this.setAttribute(tag, model);
     if (IS_HEAD && !model.content) return DOM.style(model);
     if (!model.content) {
       if (CLEAR) this.setAttribute(tag, '');
-      return Object.keys(model).forEach(key => {
-        let value = model[key];
-        value && value.binders ? value.binders.forEach(binder => binder.bind(this, key, value.onvalue)) : this.style[key] = value;
-      });
+      return Object.entries(model).forEach(([key, value]) => value && value.binders ? value.binders.forEach(binder => binder.bind(this, key, value.onvalue)) : this.style[key] = value);
     }
     if (!['boolean', 'number', 'string'].includes(typeof model.content)) model.content = DOM.css(model.content);
   }
@@ -84,14 +75,7 @@ Element.prototype.create = function (model, ...args) {
   }
   if (IS_PRIMITIVE) {
     if (IS_HEAD) {
-      const type = {
-        css: 'stylesheet',
-        sass: 'stylesheet/sass',
-        scss: 'stylesheet/scss',
-        less: 'stylesheet/less',
-        js: 'text/javascript',
-        ico: 'icon'
-      } [typeof model === 'string' ? model.split('.').slice(-1)[0] : 'none'];
+      const type = DOM.getType(model);
       if (tag === 'title') return this.innerHTML += `<title>${model}</title>`;
       if (tag === 'icon') return this.innerHTML += `<link rel="icon" href="${model}">`;
       if (tag === 'charset') return this.innerHTML += `<meta charset="${model}">`;
@@ -112,8 +96,9 @@ Element.prototype.create = function (model, ...args) {
       }, 'script');
     }
     let done = (this.style[station] !== undefined) ? (this.style[station] = model) : undefined;
-    if (['accept', 'accept-charset', 'accesskey', 'action', 'align', 'alt', 'async', 'autocomplete', 'autofocus', 'autoplay', 'bgcolor', 'border', 'charset', 'checked', 'cite', 'class', 'color', 'cols', 'colspan', '*content', 'contenteditable', 'controls', 'coords', 'data', 'datetime', 'default', 'defer', 'dir', 'dirname', 'disabled', 'download', 'draggable', 'enctype', 'for', 'form', 'formaction', 'headers', 'height', 'hidden', 'high', 'href', 'hreflang', 'http-equiv', 'id', 'ismap', 'kind', 'lang', 'list', 'loop', 'low', 'max', 'maxlength', 'media', 'method', 'min', 'multiple', 'muted', 'name', 'novalidate', 'open', 'optimum', 'pattern', 'placeholder', 'poster', 'preload', 'readonly', 'rel', 'required', 'reversed', 'rows', 'rowspan', 'sandbox', 'scope', 'selected', 'shape', 'size', 'sizes', 'spellcheck', 'src', 'srcdoc', 'srclang', 'srcset', 'start', 'step', 'style', 'tabindex', 'target', 'title', 'translate', 'type', 'usemap', 'value', 'wrap', 'width'].includes(station)) done = !this.setAttribute(station.replace('*', ''), model);
-    if (station === 'id') addID(model, this);
+    station = station.replace('*', ''); // disambiguate 
+    if (DOM.isAttribute(station)) done = !this.setAttribute(station, model);
+    if (station === 'id') DOM.addID(model, this);
     if (done !== undefined) return;
   }
   let elem = (model.tagName || model.elt) ? model : false;
@@ -173,8 +158,8 @@ class Binder {
     this._listeners[this._listeners] = func;
     return this._listenerCount++;
   }
-  removeListener(key) {
-    delete this._listeners[key];
+  removeListener(countIndex) {
+    delete this._listeners[countIndex];
   }
   bind(...args) {
     let target = args.filter(a => a.tagName || a._bonds)[0];
@@ -204,7 +189,6 @@ class Binder {
 
 // global static methods to handle the DOM
 class DOM {
-
   // created the element and props in the  body or an element passed
   static create(...args) {
     let elt = args.filter(a => a && a.tagName)[0];
@@ -212,7 +196,6 @@ class DOM {
     if (elt) return elt.create(...args);
     window.addEventListener('load', _ => DOM.create(...args));
   }
-
   // returns a bind for element's props to use ONLY whithin a create() model
   static bind(binders, onvalue = v => v) {
     if (!Array.isArray(binders)) binders = [binders];
@@ -222,7 +205,6 @@ class DOM {
       onvalue: _ => onvalue(...binders.map(binder => binder.value))
     }
   }
-
   // returns a new bind for element's props whithin a create() model, to be updated after a XMLHttpRequest
   static load(url, onload, parseJSON = false) {
     let binder = new Binder();
@@ -230,12 +212,10 @@ class DOM {
     DOM.request(url, data => data !== undefined ? binder.value = parseJSON ? JSON.parse(data) : data : null);
     return obj;
   }
-
   // returns a new bind for element's props whithin a create() model, to be updated after a JSON PARSED XMLHttpRequest
   static loadJSON(url, onload) {
     return DOM.load(url, onload, true);
   }
-
   // makes a XMLHttpRequest using POST, make (data = false) for GET method
   static request(url, data, onsuccess = _ => null, onerror = _ => null) {
     if (!url) return;
@@ -250,7 +230,6 @@ class DOM {
     xobj.open(GET ? 'POST' : 'GET', url, true);
     xobj.send(data);
   }
-
   // same as before bu terurns a JSON object
   static requestJSON(url, data, onsuccess = _ => null, onerror = _ => null) {
     if (typeof data === 'function') {
@@ -260,7 +239,6 @@ class DOM {
     }
     DOM.request(url, data, d => onsuccess(JSON.parse(d)), onerror);
   };
-
   // adds styles to the head as global CSS
   static style(style) {
     if (window.domstyleElem === undefined) {
@@ -313,11 +291,8 @@ class DOM {
       content: style
     }, 'style');
   }
-
-  /* converts JSON to CSS, nestings and all. 
-  The model can also have id: and class: properties to be added to the selector.
-  "_" in selectors are turned into ".". 
-  When nesting: use a trailing "_" to affect any selector under the parent, instead of default immediate child. or add an "all: true" property.*/
+  /* converts JSON to CSS, nestings and all. Models can have id: & class: properties to be added to the selector.
+  "_" in selectors are turned into ".". Use a trailing "_" to affect any selector under the parent, instead of default immediate child (>), or add an "all: true" property.*/
   static css(sel, model) {
     const assignAll = (arr = [], dest = {}) => {
       arr.forEach(prop => Object.assign(dest, prop));
@@ -327,9 +302,8 @@ class DOM {
       if (!sel) return;
       if (Array.isArray(sel)) sel = assignAll(sel);
       if (sel.tag || sel.id || sel.class) return DOM.css(sel.tag ? sel.tag : '', sel);
-      return Object.keys(sel).map(key => DOM.css(key, sel[key])).join(' ');
+      return Object.entries(sel).map(([key, value])  => DOM.css(key, value)).join(' ');
     }
-    const unCamel = (str) => str.replace(/([A-Z])/g, '-' + '$1').toLowerCase();
     let extra = [];
     let cls = sel.split('_');
     sel = cls.shift();
@@ -339,21 +313,20 @@ class DOM {
       cls = [];
     }
     if (sel.toLowerCase() === 'fontface') sel = '@font-face';
-    if (['boolean', 'number', 'string'].includes(typeof model)) return `${unCamel(sel)}: ${model};\n`;
+    if (DOM.isPrimitive(model)) return `${DOM.unCamel(sel)}: ${model};\n`;
     if (Array.isArray(model)) model = assignAll(model);
     if (model.class) cls.push(...model.class.split(' '));
     if (model.id) sel += '#' + model.id;
     delete model.class;
     delete model.id;
     if (cls.length) sel += '.' + cls.join('.');
-    let css = Object.keys(model).map(key => {
-      let style = model[key];
+    let css = Object.entries(model).map(([key, style]) => {
       if (style === undefined || style === null) return;
-      if (['boolean', 'number', 'string'].includes(typeof style)) return DOM.css(key, style);
-      let sub = unCamel(key.split('(')[0]);
+      if (DOM.isPrimitive(style)) return DOM.css(key, style);
+      let sub = DOM.unCamel(key.split('(')[0]);
       let xSel = `${sel}>${key}`;
-      if (['active', 'checked', 'disabled', 'empty', 'enabled', 'first-child', 'first-of-type', 'focus', 'hover', 'in-range', 'invalid', 'last-of-type', 'link', 'only-of-type', 'only-child', 'optional', 'out-of-range', 'read-only', 'read-write', 'required', 'root', 'target', 'valid', 'visited', 'lang', 'not', 'nth-child', 'nth-last-child', 'nth-last-of-type', 'nth-of-type'].includes(sub)) xSel = `${sel}:${sub}`;
-      else if (['after', 'before', 'first-letter', 'first-line', 'selection'].includes(sub)) xSel = `${sel}::${sub}`;
+      if (DOM.isPseudoClass(sub)) xSel = `${sel}:${sub}`;
+      else if (DOM.isPseudoElement(sub)) xSel = `${sel}::${sub}`;
       else if (['_', '.'].some(s => key.startsWith(s))) xSel = `${sel}${sub}`;
       else if (['_', '.'].some(s => key.endsWith(s)) || style.all) xSel = `${sel} ${sub}`;
       delete style.all;
@@ -361,7 +334,6 @@ class DOM {
     }).join(' ');
     return (css ? `\n${sel} {\n ${css}}` : '') + extra.join(' ');
   }
-
   //creates an element and returns the html code for it
   static html(model, tag = 'div') {
     let output;
@@ -372,19 +344,29 @@ class DOM {
     document.body.removeChild(elt);
     return output;
   }
-
   // initializes the head and body from model with initial values or json file
+  static getSetup = _ => {
+    let ini = document.head.querySelector('[create]');
+    if (ini) return (ini.getAttribute('create'));
+    ini = document.head.querySelector('create');
+    if (ini) DOM.setup(ini.innerHTML);
+  }
   static setup(ini) {
-    if (ini === undefined) return;
-    if (typeof ini === 'boolean' || !ini.length) ini = {};
+    if ([undefined, null, false].includes(ini)) return;
+    if ([true, ''].includes(ini)) ini = {};
     if (typeof ini === 'string') {
-      if (ini.endsWith('.json')) return DOM.request(ini, data => DOM.setup(data));
-      ini = JSON.parse(ini);
+      if (ini.endsWith('.json')) return DOM.requestJSON(ini, data => DOM.setup(data));
+      try {
+        ini = JSON.parse(ini);
+      } catch (e) {
+        console.log('Unable to parse DOM setup.', e);
+        ini = {};
+      }
     }
     const INI = {
-      title: 'A Domified Site',
+      title: 'DOM.created',
       charset: 'UTF-8',
-      viewport: 'width=device-width, minimum-scale=1, maximum-scale=1',
+      viewport: 'width=device-width',
       icon: false,
       meta: [],
       link: [],
@@ -393,53 +375,71 @@ class DOM {
       style: [],
       css: [],
       script: [],
-      entry: false,
+      entry: 'main.js',
       module: true,
       postscript: []
     };
-    const rename = (obj, name, newName) => {
-      if (obj[name] === undefined) return;
-      if (Array.isArray(name)) return name.forEach((n, i) => rename(obj, n, newName[i]));
-      obj[newName] = obj[name];
-      delete obj[name];
-    } // renames ini props to avoid misnaming of misspells
-    rename(ini, ['fontFace', 'fontface', 'fonts', 'links', 'entryPoint', 'entryPoint'],
-      ['font', 'font', 'font', 'link', 'entry', 'entry']);
+    ini = Object.entries(ini).forEach(([key, value]) => {
+      delete ini[key];
+      ini[key.toLocaleLowerCase()] = value;
+    });
+    DOM.rename(ini, ['fontface', 'fonts', 'links', 'entrypoint'], ['font', 'font', 'link', 'entry']);
     let settings = Object.assign({}, INI); // combines ini and INI into settings
     Object.assign(settings, ini);
-    const asArray = foo => Array.isArray(foo) ? foo : [foo];
     document.head.create({
       title: settings.title,
       charset: settings.charset,
       viewport: settings.viewport,
       icon: settings.icon,
       font: settings.font,
-      meta: asArray(settings.meta),
-      link: asArray(settings.link),
-      script: asArray(settings.script)
+      meta: DOM.asArray(settings.meta),
+      link: DOM.asArray(settings.link),
+      script: DOM.asArray(settings.script)
     });
-    DOM.style([asArray(settings.style), asArray(settings.css)]);
+    DOM.style([DOM.asArray(settings.style), DOM.asArray(settings.css)]);
     settings.entry = !settings.entry ? undefined : new Object({
       type: settings.module ? 'module' : undefined,
       src: settings.entry
     })
     DOM.create({
-      script: [settings.entry, ...asArray(settings.postscript)]
+      script: [settings.entry, ...DOM.asArray(settings.postscript)]
     });
-    // anything else passed in ini will become a body prop
+    // anything else passed in ini becomes a body prop
     Object.keys(ini).filter(key => INI[key] !== undefined).forEach(key => delete ini[key]);
     DOM.create(ini);
   }
-
   static querystring() {
     var qs = location.search.substring(1);
     if (!qs) return Object();
     if (qs.includes('=')) return JSON.parse('{"' + decodeURI(location.search.substring(1)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
     return qs.split('/');
   }
-
+  // auxiliary methods
+  static addID = (id, elt) => {
+    if (Array.isArray(elt)) return elt.forEach(e => addID(id, e));
+    if (!window[id]) return window[id] = elt;
+    if (Array.isArray(window[id])) return window[id].push(elt);
+    window[id] = [window[id], elt];
+  };
+  static isPrimitive = foo => ['boolean', 'number', 'string'].includes(typeof foo);
+  static asArray = foo => Array.isArray(foo) ? foo : [foo];
+  static unCamel = str => str.replace(/([A-Z])/g, '-' + '$1').toLowerCase();
+  static isAttribute = str => ['accept', 'accept-charset', 'accesskey', 'action', 'align', 'alt', 'async', 'autocomplete', 'autofocus', 'autoplay', 'bgcolor', 'border', 'charset', 'checked', 'cite', 'class', 'color', 'cols', 'colspan', 'content', 'contenteditable', 'controls', 'coords', 'data', 'datetime', 'default', 'defer', 'dir', 'dirname', 'disabled', 'download', 'draggable', 'enctype', 'for', 'form', 'formaction', 'headers', 'height', 'hidden', 'high', 'href', 'hreflang', 'http-equiv', 'id', 'ismap', 'kind', 'lang', 'list', 'loop', 'low', 'max', 'maxlength', 'media', 'method', 'min', 'multiple', 'muted', 'name', 'novalidate', 'open', 'optimum', 'pattern', 'placeholder', 'poster', 'preload', 'readonly', 'rel', 'required', 'reversed', 'rows', 'rowspan', 'sandbox', 'scope', 'selected', 'shape', 'size', 'sizes', 'spellcheck', 'src', 'srcdoc', 'srclang', 'srcset', 'start', 'step', 'style', 'tabindex', 'target', 'title', 'translate', 'type', 'usemap', 'value', 'wrap', 'width'].includes(str);
+  static isPseudoClass = str => ['active', 'checked', 'disabled', 'empty', 'enabled', 'first-child', 'first-of-type', 'focus', 'hover', 'in-range', 'invalid', 'last-of-type', 'link', 'only-of-type', 'only-child', 'optional', 'out-of-range', 'read-only', 'read-write', 'required', 'root', 'target', 'valid', 'visited', 'lang', 'not', 'nth-child', 'nth-last-child', 'nth-last-of-type', 'nth-of-type'].includes(str);
+  static isPseudoElement = str => ['after', 'before', 'first-letter', 'first-line', 'selection'].includes(str);
+  static getType = str => new Object({
+    css: 'stylesheet',
+    sass: 'stylesheet/sass',
+    scss: 'stylesheet/scss',
+    less: 'stylesheet/less',
+    js: 'text/javascript',
+    ico: 'icon'
+  })[typeof str === 'string' ? str.split('.').slice(-1)[0] : 'none'];
+  static rename = (obj, name, newName) => {
+    if (obj[name] === undefined) return;
+    if (Array.isArray(name)) return name.forEach((n, i) => rename(obj, n, newName[i]));
+    obj[newName] = obj[name];
+    delete obj[name];
+  }
 }
-
-// calls DOM.setup() is the has an "setup" attribute anywhere in the head
-let ini = document.head.querySelector('[setup]');
-if (ini) DOM.setup(ini.getAttribute('setup'));
+DOM.getSetup();
