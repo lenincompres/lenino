5492fa36906b853f55f270b3eea846dc3d19998e
Lenin Compres <lenincompres@gmail.com>
2021-12-14 16:30:27 -0500

    touches

--- projects/pre/src/cube.js
+++ projects/pre/src/cube.js
@@ -6,91 +6,85 @@ const POSTS = 5;
 const RADIUS = 40;
 
 function Cube({
-  onready = () => null,
-  onclick = () => null,
-  container = document.body,
-  center = '000',
+  container,
   lang = ENG,
-  vicinity = false,
   ref = undefined,
   noLabels = false,
+  onready = () => null,
+  onclick = () => null,
 }) {
   let me = new p5(function () {});
-  me.vicinity = vicinity;
-  me.ref = ref;
-  me.isHover = false;
-  me.nextPost = Math.floor(Math.random() * POSTS);
-  me.currentPost = 0;
-  me.changePost = false;
-  me.centerCode = center;
-  me.onclick = onclick;
-  me.onready = onready;
-  me.lang = lang !== undefined ? lang : ENG;
+  let isHover = false;
+  let nextPost = Math.floor(Math.random() * POSTS);
+  let currentPost = 0;
+  let changePost = false;
+  let overState;
+
+  let states = new Array(27).fill().map((_, i) => new State({
+    sketch: me,
+    index: i
+  })).filter(state => !me.vicinity || state.isNear()).sort(state => state.tier);
+
+  if (ref) states.forEach(state => state.setRef(ref));
+
+  let size = 0.4 * states[0].radius;
 
   me.setup = function () {
     let canvas = me.createCanvas(me.windowWidth, 400);
     me.center = [me.width * 0.5, me.height * 0.5];
     if (container) container.create({
       content: canvas.elt,
-      onready: me.onready
+      onready: onready
     }, true);
 
     me.strokeWeight(3);
     me.textFont('Verdana');
-    me.textSize(me.size);
+    me.textSize(size);
     me.textAlign(me.CENTER, me.CENTER);
-
-    me.states = new Array(27).fill().map((_, i) => new State({
-      sketch: me,
-      index: i
-    })).filter(state => !me.vicinity || state.isNear()).sort(state => state.tier);
-    me.size = 0.4 * me.states[0].radius;
-
-    if (me.ref) me.states.forEach(state => state.setRef(me.ref));
     me.animate();
   }
 
   me.draw = function () {
     me.clear();
     me.translate(...me.center);
-    me.states.forEach(s => {
-      s.setRef(me.overState);
-      s.interact = !!me.overState;
+    states.forEach(s => {
+      s.setRef(overState);
+      s.interact = !!overState;
       s.draw()
     });
-    if (me.overState) {
-      let c = me.color('#' + me.overState.info.code.codeToHex())
+    if (overState) {
+      let c = me.color('#' + overState.info.code.codeToHex())
       let l = me.lightness(c) < 45 || me.green(c) < 45;
       me.stroke(c);
       me.fill(l ? 255 : 0);
-      me.text(me.overState.info.archetype, me.mouseX - me.center[0], me.mouseY - me.center[1] - me.size);
+      me.text(overState.info.archetype, me.mouseX - me.center[0], me.mouseY - me.center[1] - size);
     }
-    if (me.changePost) {
-      me.states.forEach(s => s.post = s.post === 0 ? me.nextPost : 0);
-      me.currentPost = me.states[0].post;
+    if (changePost) {
+      states.forEach(s => s.post = s.post === 0 ? nextPost : 0);
+      currentPost = states[0].post;
       me.animate();
-      me.changePost = false;
+      changePost = false;
     }
-    if (!me.currentPost || noLabels) return; // not to draw the labels
+    if (!currentPost || noLabels) return; // not to draw the labels
     let y = RADIUS * 4.5;
     let x = RADIUS * 4.75;
-    const esp = me.lang === ESP;
+    const esp = lang === ESP;
     let texts = [
       [esp ? 'Activo' : 'Active', -x, y],
       [esp ? 'Atento' : 'Attentive', 0, y],
       [esp ? 'Introspectivo' : 'Introspective', x, y]
     ];
-    if (me.currentPost === 2) texts = [
+    if (currentPost === 2) texts = [
       [esp ? 'Instintivo' : 'Instinctive', -x * 1.12, y],
       [esp ? 'Informativo' : 'Informative', 0, y],
       [esp ? 'Regulativo' : 'Regulative', x * 1.12, y]
     ];
-    else if (me.currentPost === 3) texts = [
+    else if (currentPost === 3) texts = [
       [esp ? 'Objetivo' : 'Objective', -x, y],
       [esp ? 'Sensible' : 'Responsive', 0, y],
       [esp ? 'Affectivo' : 'Affective', x, y]
     ];
-    else if (me.currentPost === 4) texts = [
+    else if (currentPost === 4) texts = [
       ['Base', -x * 1.28, y],
       [esp ? 'Periféricos & Neutro' : 'Peripheral & Neutral', 0, y],
       [esp ? 'Tope' : 'Top', x * 1.28, y]
@@ -103,50 +97,50 @@ function Cube({
 
   me.animate = function (wait = 1000) {
     if (me.timeOut) clearTimeout(me.timeOut);
-    if (me.freeze && me.currentPost === me.nextPost) return;
-    let isIni = me.currentPost !== 0 || me.nextPost === 0;
-    me.timeOut = setTimeout(() => me.changePost = !me.isHover, wait * (isIni ? WAIT : 0.5));
-    if (isIni) me.nextPost = (me.nextPost + 1) % POSTS;
+    if (me.freeze && currentPost === nextPost) return;
+    let isIni = currentPost !== 0 || nextPost === 0;
+    me.timeOut = setTimeout(() => changePost = !isHover, wait * (isIni ? WAIT : 0.5));
+    if (isIni) nextPost = (nextPost + 1) % POSTS;
   }
 
   me.view = function (view) {
-    me.changePost = true;
+    changePost = true;
     if (isNaN(view)) {
-      me.states.forEach(s => s.hidden = false);
+      states.forEach(s => s.hidden = false);
       me.freeze = false;
-      me.nextPost = (me.nextPost + 1) % POSTS;
+      nextPost = (nextPost + 1) % POSTS;
       me.animate();
       return;
     }
     me.freeze = true;
-    me.states.forEach(s => s.hidden = view < 0 && !s.isRim && s.level - 3 > 2 * view);
-    me.nextPost = view < 0 ? 0 : parseInt(view);
+    states.forEach(s => s.hidden = view < 0 && !s.isRim && s.level - 3 > 2 * view);
+    nextPost = view < 0 ? 0 : parseInt(view);
   }
 
   me.mouseMoved = function () {
     if (!me.canvas) return;
-    me.isHover = !!parseInt(me.get(me.mouseX, me.mouseY).join('')); // any pixel color under the mouse
-    me.cursor(me.isHover ? me.HAND : me.ARROW);
-    if (me.isHover) {
-      let newOver = me.states.filter(s => !s.hidden && me.dist(me.mouseX - me.center[0], me.mouseY - me.center[1], ...s.coords) < s.radius).pop();
-      if (newOver === me.overState) return;
-      if (me.overState) me.overState.selected = false;
-      me.overState = newOver;
-      if (me.overState) me.overState.selected = true;
-      if (me.onmouseover) me.onmouseover(me.overState);
+    isHover = !!parseInt(me.get(me.mouseX, me.mouseY).join('')); // any pixel color under the mouse
+    me.cursor(isHover ? me.HAND : me.ARROW);
+    if (isHover) {
+      let newOver = states.filter(s => !s.hidden && me.dist(me.mouseX - me.center[0], me.mouseY - me.center[1], ...s.coords) < s.radius).pop();
+      if (newOver === overState) return;
+      if (overState) overState.selected = false;
+      overState = newOver;
+      if (overState) overState.selected = true;
+      if (me.onmouseover) me.onmouseover(overState);
     } else {
-      if (me.overState) {
-        me.overState.selected = false;
-        if (me.onmouseout) me.onmouseout(me.overState);
+      if (overState) {
+        overState.selected = false;
+        if (me.onmouseout) me.onmouseout(overState);
         me.animate();
       }
-      me.overState = false;
+      overState = false;
     }
   }
 
   me.mouseReleased = function () {
-    if (!me.isHover) return;
-    if (me.onclick) me.onclick(me.overState);
+    if (!isHover) return;
+    onclick(overState);
   }
 
   return me;
