b148adca7091566f495b5ab54d093845c8ed0d05
Lenin Compres <lenincompres@gmail.com>
2022-12-13 12:16:52 -0500

    show ready

--- strikeout/BLE.js
+++ strikeout/BLE.js
@@ -1,12 +1,16 @@
 // The serviceUuid must match the serviceUuid of the device you would like to connect
 let serviceUuid;
+let myOnValue = () => null;
+let myCharacteristic;
+let myValue = 0;
 let myBLE;
 let isConnected = false;
 
 function setupBLE(uid, onData = () => null){
   // Create a p5ble class
   myBLE = new p5ble();
-  serviceUuid = uid;
+  serviceUuid = uid.toLowerCase();
+  myOnValue = onData;
 
   // Create a 'Connect' button
   const connectButton = createButton('Connect')
@@ -15,6 +19,8 @@ function setupBLE(uid, onData = () => null){
   // Create a 'Disconnect' button
   const disconnectButton = createButton('Disconnect')
   disconnectButton.mousePressed(disconnectToBle);
+
+  return myBLE;
 }
 
 function connectToBle() {
@@ -37,11 +43,31 @@ function onDisconnected() {
 // A function that will be called once got characteristics
 function gotCharacteristics(error, characteristics) {
   if (error) console.log('error: ', error);
-  console.log('characteristics: ', characteristics);
+
+  console.log("gotCharacteristics:", characteristics);
+  
+  myCharacteristic = characteristics[0];
+  // Start notifications on the first characteristic by passing the characteristic
+  // And a callback function to handle notifications
+  //myBLE.startNotifications(myCharacteristic, onGotCharacteristics);
+
+  myBLE.read(myCharacteristic, gotValue);
 
   // Check if myBLE is connected
   isConnected = myBLE.isConnected();
 
   // Add a event handler when the device is disconnected
-  myBLE.onDisconnected(onDisconnected)
+  myBLE.onDisconnected(onDisconnected);
+}
+
+// A function that will be called once got values
+function gotValue(error, value) {
+  if (error) console.log('error: ', error);
+  else myOnValue(value);
+  if(!isConnected) return;
+  myValue = value;
+  myBLE.read(myCharacteristic, 'string', gotValue);
+  // After getting a value, call p5ble.read() again to get the value again
+  // You can also pass in the dataType
+  // Options: 'unit8', 'uint16', 'uint32', 'int8', 'int16', 'int32', 'float32', 'float64', 'string'
 }
--- strikeout/main.js
+++ strikeout/main.js
@@ -1,4 +1,14 @@
-const STATES = ["INTRO", "RESET", "READY", "PITCH", "OUT", "HIT", "OVER", "END"];
+/*
+  * Created by: Lenino (Lenin Compres)
+  * 
+*/
+
+
+// contances
+const [S_INTRO, S_RESET, S_READY, S_PITCH, S_OUT, S_HIT, S_OVER, S_END] = ["INTRO", "RESET", "READY", "PITCH", "OUT", "HIT", "OVER", "END"];
+const STATES = [S_INTRO, S_RESET, S_READY, S_PITCH, S_OUT, S_HIT, S_OVER, S_END];
+
+//variables
 let currentState = 0;
 let state = STATES[0];
 let currentQ = -1;
@@ -10,10 +20,11 @@ let counterTimeout;
 let bases = [0, 0, 0];
 let runCount = 0;
 let outCount = 0;
-let maxTime = 2000;
 let visuals = [];
-let batData = {};
 let batIsUp = null;
+let hitValue = 0;
+let serialBLE;
+let quiz;
 
 function preload() {
   titleFont = loadFont('assets/Sportypo.ttf');
@@ -24,9 +35,11 @@ function preload() {
   outImg = loadImage('assets/out.png');
   hitImg = loadImage('assets/hit.png');
   diamondImg = loadImage('assets/diamond.png');
+  introSound = loadSound('assets/baseball.mp3');
   hitSound = loadSound('assets/hit.mp3');
   swingSound = loadSound('assets/swing.wav');
   catchSound = loadSound('assets/catch.mp3');
+  clapsSound = loadSound('assets/claps.mp3');
 }
 
 function setup() {
@@ -34,39 +47,65 @@ function setup() {
   W = windowWidth;
   H = W / ratio;
   if (H > windowHeight) {
-    H = windowHeight * 0.9;
+    H = windowHeight * 0.8;
     W = ratio * H;
   }
   M = 5 * H / 60;
   W2 = 0.5 * W;
   H2 = 0.5 * H;
+
+  let isWired = !!DOM.querystring().wired;
+
   DOM.set({
     background: "darkgreen",
     textAlign: "center",
+    fontSize: (0.25 * M) + "px",
     canvas: createCanvas(W, H),
     footer: {
+      margin: "0 0 1em",
       color: "#9f9",
       p: [
         "Created by Lenino for ITP TISCH NYU • Physical Computing with Tom Igoe.",
         "Brooklyn, NY. 2022"
-      ]
+      ],
+      a: {
+        color: "#f99",
+        display: "block",
+        text: "Trivia from Travels With Elle",
+        href: "https://travelswithelle.com/other/multiple-choice-trivia-questions/",
+        target: "_blank",
+      },
+      button: {
+        position: "fixed",
+        backgroundColor: isWired ? "silver" : "#99f",
+        color: isWired ? "black" : "#00f",
+        top: 0,
+        right: 0,
+        margin: "1em",
+        text: isWired ? "Wired" : "Wireless",
+        click: () => {
+          let url = window.location.href.split("?")[0];
+          console.log(url);
+          window.location.href = isWired ? url : url + "?wired=true";
+        },
+      }
     }
   });
   textAlign(CENTER, CENTER);
 
-  if (webSerialSetup) webSerialSetup(data => {
-    if (data.indexOf("bat: ") >= 0) {
-      batUpdate(...data.split(" ")[1].split(","));
-    } else {
-      console.log(data);
-    }
-  });
-
-  /*if (setupBLE) setupBLE("19B10010-E8F2-537E-4F6C-D104768A1214", data => {
-    console.log("BLE data:", data);
-  });*/
-
-  quiz.sort(() => (Math.random() > .5) ? 1 : -1);
+  if (DOM.querystring().wired) {
+    if (webSerialSetup) webSerialSetup(data => {
+      if (data && data.indexOf("bat: ") >= 0) {
+        batUpdate(data);
+      }
+    });
+  } else {
+    if (setupBLE) setupBLE("7DEF8317-7300-4EE6-8849-46FACE74CA2A", data => {
+      if (data && data.indexOf("bat: ") >= 0) {
+        batUpdate(data);
+      }
+    });
+  }
 
   graph = Mover(W2, H2, () => {
     let img = [null, resetImg, readyImg, null, outImg, hitImg, null, null][currentState];
@@ -78,44 +117,16 @@ function setup() {
   title = Mover(W2, H2, () => {
     textFont(titleFont);
     fill("darkred");
-    textSize(state === "OVER" || state === "INTRO" ? 1.1 * M : 0.8 * M);
-    text(state === "OVER" ? "Game Over" : "Strikeout Trivia", 0, 0);
+    textSize(state === S_OVER || state === "INTRO" ? 1.1 * M : 0.8 * M);
+    text(state === S_OVER ? "Game Over" : "Strikeout Trivia", 0, 0);
   }, () => {
-    let isStart = state === "INTRO" || state === "OVER" || state === "END";
+    let isStart = state === "INTRO" || state === S_OVER || state === S_END;
     title.moveTo(isStart ? W2 : 0.3 * W, isStart ? H2 : 1.3 * M);
   });
   visuals.push(title);
 
-  headline = Mover(W2, 0.6 * H, () => {
-    let theValue = ["hit", "double", "triple", "homerun"][question.value - 1];
-    if (state === "PITCH") {
-      textSize(0.5 * M);
-      textFont(numberFont);
-      fill(["white", "darkgreen", "green", "limegreen", "lime"][question.value - 1]);
-      text(theValue, 0, -0.8 * M);
-    }
-    fill(48);
-    noStroke();
-    textFont("arial");
-    textSize(0.68 * M);
-    let promtText = [
-      "by Lenino",
-      "Lower bat ↓",
-      "Get ready ↑",
-      question.question,
-      "You're out!",
-      `It's a ${theValue}!`,
-      "Try again",
-      `It's a ${theValue}, and you won!`
-    ][currentState];
-    text(promtText, 0, 0);
-  }, () => {
-    headline.moveToY(H * [0.6, 0.7, 0.7, 0.3, 0.72, 0.72, 0.6, 0.6][currentState]);
-  });
-  visuals.push(headline);
-
   balls = Array(4).fill().map((_, i) => Mover(-2 * M, H - 2 * M, () => {
-    if (state !== "PITCH") return;
+    if (state !== S_PITCH) return;
     fill(86);
     noStroke();
     let [isLess, isSame] = [currentA < i, currentA === i];
@@ -129,7 +140,7 @@ function setup() {
   visuals.push(...balls);
 
   pitch = Mover(W2, 0.44 * H, () => {
-    if (!question.answers || state !== "PITCH") return;
+    if (!question.answers || state !== S_PITCH) return;
     fill("black");
     noStroke();
     textSize(M);
@@ -137,16 +148,20 @@ function setup() {
   });
   visuals.push(pitch);
 
-  counter = Mover(W2, 0.83 * H, () => {
-    if (state !== "READY" && state !== "PITCH") return;
-    if (state === "PITCH" && currentA < 0) return;
-    if (countNumber <= 0 || countNumber > 3) return;
+  counter = Mover(W2, 0.8 * H, () => {
+    if (state !== S_READY && state !== S_PITCH) return;
+    if (countNumber <= 0) return;
     textFont(numberFont);
-    let isPitch = currentA >= 0;
-    fill(isPitch ? "darkred" : "gray");
     noStroke();
     textSize(1.5 * M);
-    text(countNumber, 0, 0);
+    let isPitch = currentA >= 0;
+    fill("gray");
+    if (isPitch) {
+      let theColor = color("darkred");
+      theColor.setAlpha(countNumber * 255 / 4);
+      fill(theColor);
+    }
+    text(countNumber > 3 ? "" : countNumber, 0, 0);
   });
   visuals.push(counter);
 
@@ -168,31 +183,62 @@ function setup() {
     });
     //runs
     noStroke();
-    fill(state === "OVER" || state === "INTRO" ? 210 : "white");
+    fill("white");
     circle(0, 2 * d, 0.8 * M);
     textSize(runCount < 10 ? M : 0.6 * M);
     fill("green");
     textFont(numberFont);
     text(runCount, 0, 2 * d);
+    //pitch value 
+    if(state !== S_PITCH) return;
+    textFont(numberFont);
+    let theText = ["", "hit", "double", "triple", "homerun"][question.value];
+    let theColor = ["", "", "darkgreen", "green", "limegreen"][question.value];
+    fill(theColor);
+    textSize(0.5 * M);
+    text(theText, 0, 2.7 * d);
   });
   visuals.push(diamond);
+
+  headline = Mover(W2, 0.6 * H, () => {
+    if (state === S_PITCH) textAlign(CENTER, TOP);
+    let theValue = ["", "hit", "double", "triple", "homerun"][hitValue];
+    fill(48);
+    noStroke();
+    textFont("arial");
+    textSize(0.68 * M);
+    let promtText = [
+      "by Lenino",
+      "Lower bat ↓",
+      "Get ready ↑",
+      question.question,
+      "You're out!",
+      `It's a ${theValue}!`,
+      "Try again",
+      `It's a ${theValue}, and you won!`
+    ][currentState];
+    text(promtText, 0, 0);
+  }, () => {
+    headline.moveToY(H * [0.6, 0.7, 0.7, 0.2, 0.72, 0.72, 0.6, 0.6][currentState]);
+  });
+  visuals.push(headline);
 
   outDisplay = Mover(W - 2 * M, H - 2 * M, () => {
     // outs
     noStroke();
     fill("darkred");
     textSize(1.3 * M);
-    Array(outCount).fill().forEach((_, i) => text("✗", 0, -i * M));
+    Array(outCount).fill().forEach((_, i) => text("✗", 0, -i * 1.3 * M));
   });
   visuals.push(outDisplay);
 
-  setCounter(() => setState());
+  setCounter(() => setState(S_INTRO), 2);
 }
 
 function draw() {
   background("darkgreen");
   noStroke();
-  fill(state === "INTRO" || state === "OVER" ? 210 : 255);
+  fill("white");
   rect(0.5 * M, 0.5 * M, W - M, H - M, M);
   visuals.forEach(v => v.draw());
 }
@@ -201,58 +247,41 @@ function draw() {
 
 let batTop;
 
-function batUpdate(h, p, r, xAcc, yAcc, zAcc, xGyro, yGyro, zGyro) {
-  batData = {
-    head: h,
-    pitch: p,
-    roll: r,
-    xA: xAcc,
-    yA: yAcc,
-    zA: zAcc,
-    xG: xGyro,
-    yG: yGyro,
-    zG: zGyro,
+function batUpdate(data) {
+  let [h, p, r, aX, aY, aZ, gX, gY, gZ] = data.split(" ")[1].split(",");
+  p > 20 ? batUp() : batDown();
+  if (!batIsUp) return batTop = false;
+  if (state !== S_PITCH) return;
+  if (!batTop || p > batTop.p) return batTop = {
+    p: p,
+    h: h,
+    r: r,
   };
-  p > 10 ? batUp() : batDown();
-  if (batIsUp && state === "PITCH") {
-    if (!batTop) batTop = [h, p];
-    else if (p > batTop[0]) batTop = [h, p];
-    else if (dist(...batTop, h, p) > 16) {
-      batSwing();
-      batTop = false;
-    }
-  }
+  if (dist(batTop.h, batTop.r, h, r) > 150) batSwing();
 }
 
 function batDown() {
-  if (batIsUp !== null && !batIsUp) return;
+  if (batIsUp === false) return;
+  batSwing();
   batIsUp = false;
-  batSwitched();
-}
-
-function batUp() {
-  if (batIsUp !== null && batIsUp) return;
-  batIsUp = true;
-  batSwitched();
-}
-
-function batSwitched() {
-  if (batIsUp) {
-    if (state === "READY") setCounter(() => setState("PITCH"), 3);
-    if (state === "RESET" || state === "READY") headline.moveToY(0.7 * H);
-  } else {
-    if (state === "RESET" || state === "READY") {
-      headline.moveToY(0.75 * H);
-      state === "RESET" ? setState("READY") : stopCounter();
-    }
+  if (state === S_RESET || state === S_READY) {
+    headline.moveToY(0.75 * H);
+    state === S_RESET ? setState(S_READY) : stopCounter();
   }
 }
 
+function batUp() {
+  if (batIsUp === true) return;
+  batIsUp = true;
+  if (state === S_READY) setCounter(() => setState(S_PITCH), 3);
+  if (state === S_RESET || state === S_READY) headline.moveToY(0.7 * H);
+}
+
 function batSwing() {
-  if (state !== "PITCH") return;
-  let isHit = question.answers[currentA] === question.correct;
+  if (state !== S_PITCH) return;
   swingSound.play();
-  setState(isHit ? "HIT" : "OUT");
+  let isHit = question.answers[currentA] === question.correct;
+  setState(isHit ? S_HIT : S_OUT);
 }
 
 // FUNCTIONS
@@ -262,59 +291,73 @@ function setState(n) {
   if (n === undefined) n = (currentState + 1) % STATES.length;
   currentState = n;
   state = STATES[n];
-  if (state === "INTRO") {
+  if (state === S_INTRO) {
     bases = [0, 0, 0];
     outCount = 0;
     runCount = 0;
-    setCounter(() => setState(2));
-  } else if (state === "RESET") {
-    if (batIsUp === false) setTimeout(() => setState("READY"), 100);
-  } else if (state === "READY") {
+    introSound.play();
+    fetch('./quiz.json')
+      .then((response) => response.json())
+      .then((json) => {
+        quiz = json;
+        console.log("quiz:", quiz);
+        quiz.sort(() => (Math.random() > .5) ? 1 : -1);
+        setCounter(() => setState(S_READY), 3);
+      });
+  } else if (state === S_RESET) {
+    if (batIsUp === false) setTimeout(() => setState(S_READY), 100);
+  } else if (state === S_READY) {
     currentA = -1;
-  } else if (state === "PITCH") {
+    batIsUp = false;
+  } else if (state === S_PITCH) {
     currentQ = (currentQ + 1) % quiz.length;
     let q = quiz[currentQ];
-    if (typeof q.value !== "number" || q.value < 1) q.value = 1;
+    let defVal = random([1,1,1,1,2,2,2,3,3,4]);
+    let theQuestion = q.question;
+    if(theQuestion.length > 45){
+      theQuestion = theQuestion.split(" ").map((word,i,arr) => i === round(arr.length/2) ? ("\n" + word) : word).join(" ");
+    }
     question = {
-      question: q.question,
+      question: theQuestion,
       answers: [...q.answers],
       correct: q.answers[0],
-      value: q.value,
+      value: q.value ? q.value : defVal,
     };
     question.answers.sort(() => (Math.random() > .5) ? 1 : -1);
     balls.forEach((b, i) => setTimeout(() => b.moveTo(2 * M), i * 60));
-    setCounter(() => pitchBall(), 3);
-  } else if (state === "OUT" || state === "HIT") {
+    setCounter(() => pitchBall());
+  } else if (state === S_OUT || state === S_HIT) {
     currentA = -1;
-    if (state === "HIT") {
+    if (state === S_HIT) {
       hitSound.play();
-      bases.unshift(...Array(question.value - 1).fill(0), 1);
+      hitValue = question.value;
+      bases.unshift(...Array(hitValue - 1).fill(0), 1);
       let newRunCount = bases.reduce((v, o, i) => i > 3 ? o + v : o, 0);
-      if(newRunCount > runCount) newRun(newRunCount);
+      if (newRunCount > runCount) newRun(newRunCount);
       quiz.splice(currentQ, 1);
       currentQ -= 1;
-      if (!quiz.length) return setState("END");
+      if (!quiz.length) return setState(S_END);
     } else {
       catchSound.play();
-      quiz[currentQ].value -= 1;
       outCount += 1;
-      if (outCount >= 3) return setState("OVER");
+      if (quiz[currentQ].value) quiz[currentQ].value -= 1;
+      if (outCount >= 3) return setState(S_OVER);
     }
-    setCounter(() => setState("RESET"));
-  } else if (state === "OVER") {
-    setCounter(() => location.reload(), 6);
-  } else if (state === "END") {
-    setCounter(() => location.reload(), 8);
+    setCounter(() => setState(S_READY));
+  } else if (state === S_OVER) {
+    setCounter(() => setState(S_INTRO), 4);
+  } else if (state === S_END) {
+    setCounter(() => setState(S_INTRO), 4);
   }
   visuals.forEach(v => v.update());
 };
 
-function setCounter(trigger = () => null, n = 4) {
+function setCounter(trigger = () => null, n = 3, delay = 900) {
   if (counterTimeout) clearTimeout(counterTimeout);
   countNumber = n;
   counter.update();
   if (countNumber === 0) return trigger();
-  counterTimeout = setTimeout(() => setCounter(trigger, countNumber - 1, ), maxTime / 3);
+  counterTimeout = setTimeout(() => setCounter(trigger, countNumber - 1, delay), delay);
 }
 
 function stopCounter() {
@@ -323,10 +366,10 @@ function stopCounter() {
 }
 
 function pitchBall() {
-  if (state !== "PITCH") return;
+  if (state !== S_PITCH) return;
   currentA += 1;
-  if (currentA >= question.answers.length) return setState("OUT");
-  setCounter(() => pitchBall());
+  if (currentA >= question.answers.length) return setState(S_OUT);
+  setCounter(() => pitchBall(), 3);
   balls.forEach(b => b.update());
 };
 
@@ -334,8 +377,9 @@ function imageSized(img, f) {
   image(img, -f * img.width, -f * img.height, 2 * f * img.width, 2 * f * img.height);
 }
 
-function newRun(n){
+function newRun(n) {
   diamond.moveTo(W2 + 2 * M, H2 - 2 * M);
   runCount = n;
   setTimeout(() => diamond.moveTo(W - 2 * M, M), 200);
+  clapsSound.play();
 }
