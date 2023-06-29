7e3c28e2ee4f754f54f81fd81a99c7b0145840b2
Lenin Compres <lenincompres@gmail.com>
2023-06-29 17:19:49 -0400

    blinking

--- hermanastra/main.js
+++ hermanastra/main.js
@@ -18,7 +18,7 @@ let sinopsisElement = {
     padding: "1em 1.5em",
     tag: "section",
     background: "#fec",
-    text: "En uno de sus viajes por el espacio, el tiempo y los hongos, Lenino “El Cantacuentos” conoce a una doncella desencantada cuya hermanastra cenicienta le robó al príncipe azul. Su difunto padrastro arruinó la fortuna de su madre y un joven panadero deshonró a su hermana menor. Según el pueblo, su casa se convirtió en un hostal de brujas y reinas malvadas. Lenino llega allí y sus encantos caribeños revelan el hechizo bajo el cual se encuentra la joven.",
+    text: "En uno de sus viajes por el espacio, el tiempo y los hongos, Lenino “El Cantacuentos” conoce a una doncella desencantada cuya hermanastra cenicienta le robó al príncipe azul. Su difunto padrastro arruinó la fortuna de su madre y un joven panadero deshonró a su hermana menor. Según el pueblo, su casa se convirtió en un hostal de brujas y reinas malvadas. Lenino llega con sus encantos caribeños a descubrir el hechizo bajo el cual se encuentra.",
     height: "fit-content",
   }
 };
@@ -38,13 +38,38 @@ let bioElement = {
   },
 };
 
+let blinking = (elt, t = 0.3) => {
+  let isOn = true;
+  elt.set({
+    opacity: "1",
+    transition: t + "s",
+  });
+  setInterval(() => {
+    elt.set({
+      opacity: isOn ? "0.3" : "1",
+    });
+    isOn = !isOn;
+  }, t * 1000)
+}
+
 let funcionesElement = {
   fontSize: "1.25em",
   h6: "Próxima función:",
   a: {
     href: "https://tix.do/event/lenino-y-la-hermanastra/",
     target: "_blank",
-    content: "<b>2023, 07/08</b>: Casa de Teatro, Santo Domingo. «Tickets»"
+    span: "<b>2023, 07/08</b>: Casa de Teatro, Santo Domingo.",
+    i: {
+      border: "0.5px dotted black",
+      borderBottomStyle: "none",
+      borderTopStyle: "none",
+      boxShadow: "1px 1px 2px black",
+      margin: "0.5em",
+      padding: "0.2em 0.7em",
+      background: "#fe6",
+      text: "Tickets",
+      ready: blinking
+    }
   }
 }
 
