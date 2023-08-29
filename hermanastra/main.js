c2ed4e1158438f1f5a3ea2510a285522eea86052
Lenin Compres <lenincompres@gmail.com>
2023-08-29 13:21:45 -0400

    sponsors

--- hermanastra/main.js
+++ hermanastra/main.js
@@ -6,6 +6,15 @@ import galleryModel from "./modules/gallery.js";
 import aboutModel from "./modules/about.js";
 import { COLOR, INC } from "./settings.js";
 
+const sponsors = [
+  "coopcovi.png",
+  "arte2.png",
+  "andamioscibao.png",
+  "espejo.png",
+  "la37.png",
+  "popular.png",
+];
+
 DOM.set({
   title: "Lenino & La Hermanastra",
   charset: "UTF-8",
@@ -77,7 +86,25 @@ DOM.set({
       color: COLOR.ACCENT,
       fontSize: "0.7em",
       text: "Un monólogo musical rococómico"
-    }
+    },
+  },
+  aside:{
+    overflowY: "hidden",
+    ul: {
+      id: "eltSponsors",
+      width: "100%",
+      display: "flex",
+      li: [...sponsors, ...sponsors, ...sponsors].map(u => new Object({
+        img: {
+          height: "7em",
+          src: "media/sponsors/" + u,
+        }
+      })),
+      marginLeft: INC.as(i => {
+        let w = eltSponsors.getBoundingClientRect().width;
+        return `-${w*i/100}px`;
+      }),
+    },
   },
 
   main: {
