f8cb7c694ef5372a3aa628d160fe89a023ddeedc
Lenin Compres <lenincompres@gmail.com>
2026-06-22 12:21:23 -0400

    update

--- src/pages/data/news.js
+++ src/pages/data/news.js
@@ -12,7 +12,7 @@ const newsies = [
     es: `Lenino sale de su madriguera a cantarle cuentos a las brujas, ogros, duendes y hadas que se manifiesten la noche del **3 de Noviembre** en [Cañave]({href}).`,
     en: `Lenino comes out of his burrow to sing stories to the witches, ogres, goblins and fairies that appear on the night of **November 3** at [Cañave]({href}).`,
   }),
-}, */
+},
   {
     title: Copy.text({
       es: "¡Terräfirma necesita tu ayuda!",
@@ -27,17 +27,25 @@ const newsies = [
       es: `Mi primera novela: una precuela inédita y multiétnica del País de las Maravillas.`,
       en: `My debut novel: an original and multiethnic prequel to Alice's Wonderland.`,
     }),
-  }, {
+  }, */ {
     src: "https://i.etsystatic.com/52679041/r/il/c0dc16/6069187861/il_794xN.6069187861_a4dz.jpg",
     title: Copy.text({
-      es: "Nuevo website",
-      en: "New website",
+      es: "Aventura musical",
+      en: "Musical adventure",
     }),
     url: "https://jackrabbits.lenino.net/",
     desc: Copy.text({
-      es: `**Jack Rabbits** tiene un nuevo [website y video tutorial]({href}). Adquiere una copia limitada del juego.`,
-      en: `**Jack Rabbits** has a new [website and video tutorial]({href}). Get limited copy of the game.`,
+      es: `El website de **Jack Rabbits** es también un [viaje musical]({href}). Aprende a jugar u adquiere una copia limitada del juego.`,
+      en: `**Jack Rabbits**' website is also a [musical experience]({href}). Learn to play and get a limited copy of the game.`,
     }),
+  },  {
+    src: "assets/candelaria.png",
+    title: Copy.text({
+      es: "Nuevos shows",
+      en: "New shows",
+    }),
+    url: "https://www.instagram.com/lenino",
+    desc: "",
   }
 ];
 
